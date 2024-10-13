832808f8f05ed30a205dbd4edb079edf8dac3a4d
r4v1ndu <r4v1nduu@gmail.com>
2024-10-13 15:51:00 +0530

    a

--- RSA/rsa.c
+++ RSA/rsa.c
@@ -1,152 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <openssl/evp.h>
-#include <openssl/rsa.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
+#include <iostream> 
+#include <stdlib.h> 
+#include <math.h> 
+using namespace std; 
 
-#define KEY_LENGTH 2048
-#define PUB_EXP 65537
-
-void handleErrors(void) {
-    ERR_print_errors_fp(stderr);
-    abort();
+// Function to compute the greatest common divisor (GCD) using Euclid's algorithm
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
 }
 
-EVP_PKEY *createRSA(int keyLength, int pubExp) {
-    EVP_PKEY_CTX *ctx;
-    EVP_PKEY *pkey = NULL;
-
-    ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
-    if (!ctx) handleErrors();
-
-    if (EVP_PKEY_keygen_init(ctx) <= 0) handleErrors();
-
-    if (EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, keyLength) <= 0) handleErrors();
-
-    if (EVP_PKEY_keygen(ctx, &pkey) <= 0) handleErrors();
-
-    EVP_PKEY_CTX_free(ctx);
-    return pkey;
-}
-
-void saveRSAKey(EVP_PKEY *pkey, int isPublic) {
-    FILE *fp;
-    if (isPublic) {
-        fp = fopen("public.pem", "wb");
-        PEM_write_PUBKEY(fp, pkey);
-    } else {
-        fp = fopen("private.pem", "wb");
-        PEM_write_PrivateKey(fp, pkey, NULL, NULL, 0, NULL, NULL);
+int main() {
+  // Declaration of variables for message, prime numbers, public/private keys, etc.
+  double m, p, q, e, i, d;
+  int flag;
+  
+  // Prompt user to enter the message (number) to be encrypted and decrypted
+  cout<<"Enter the number to be encrypted and decryted\n";
+  cin>>m;
+  
+  // Prompt user to enter the first prime number
+  cout<<"Enter first prime number\n";
+  cin>>p;
+  
+  // Prompt user to enter the second prime number
+  cout<<"Enter second prime number\n\n";
+  cin>>q;
+  
+  // Compute n as the product of the two prime numbers p and q
+  // n is part of the public and private keys
+  int n = p * q;
+  
+  // Compute Euler's totient function φ (phi), which is (p-1)*(q-1)
+  // φ is used to help determine the public and private keys
+  int phi = (p-1) * (q-1);
+  
+  // Choose an integer e such that 1 < e < phi and gcd(e, phi) = 1
+  // e is the public key exponent
+  for(i = 2; i < phi; i++)
+  {
+    // Ensure that e is co-prime with φ by checking if gcd(e, phi) is 1
+    if(gcd(i, phi) == 1)
+    {
+      e = i;
+      break;  // Once a valid e is found, exit the loop
     }
-    fclose(fp);
-}
-
-EVP_PKEY *loadRSAKey(int isPublic) {
-    FILE *fp;
-    EVP_PKEY *pkey = NULL;
-    if (isPublic) {
-        fp = fopen("public.pem", "rb");
-        pkey = PEM_read_PUBKEY(fp, NULL, NULL, NULL);
-    } else {
-        fp = fopen("private.pem", "rb");
-        pkey = PEM_read_PrivateKey(fp, NULL, NULL, NULL);
+  }
+  
+  // Now, we need to find the private key exponent d
+  // d is chosen such that (d * e) % phi = 1 (i.e., d is the modular inverse of e mod φ)
+  // We iterate over possible values of i to find a suitable d
+  i = 1;
+  while(true)
+  {
+    // j is calculated as (phi * i) + 1
+    // We are looking for the smallest integer d such that (j / e) is an integer
+    int j = (phi * i) + 1;
+    if(fmod(j, e) == 0)  // Check if j is divisible by e
+    {
+      d = j / e;  // If true, assign d
+      break;  // Exit the loop once a valid d is found
     }
-    fclose(fp);
-    return pkey;
+    i++;  // Increment i and continue searching for a valid d
+  }
+  
+  // Display the computed values of φ, d, and e
+  cout<<"The value of phi: "<<phi<<endl;
+  cout<<"The value of d: "<<d<<endl;
+  cout<<"The value of e: "<<e<<endl;
+  
+  // Display the public encryption key (e, n) and the private decryption key (d, n)
+  cout<<"Public encryption key: (" <<e<<" , " <<n<< ") "<<endl;
+  cout<<"Private dencryption key: (" <<d<<" , " <<n<< ") "<<endl;
+  
+  // Encryption: Compute the ciphertext c = (m^e) % n
+  // We use the message m, raised to the power of e, then modulo n
+  int cipher = fmod(pow(m, e), n);
+  cout<<"Encrypted Ciphertext : "<<cipher<<endl;
+  
+  // Decryption: Compute the plaintext m = (c^d) % n
+  // We use the ciphertext c, raised to the power of d, then modulo n
+  int plain = fmod(pow(cipher, d), n);
+  cout<<"Decrypted Plaintext : "<<plain<<endl;
+  
+  return 0;
 }
-
-void encryptFile(const char *infile, const char *outfile) {
-    FILE *in = fopen(infile, "rb");
-    FILE *out = fopen(outfile, "wb");
-    EVP_PKEY *pkey = loadRSAKey(1);  // Load public key
-
-    if (!in || !out || !pkey) handleErrors();
-
-    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(pkey, NULL);
-    if (!ctx || EVP_PKEY_encrypt_init(ctx) <= 0) handleErrors();
-
-    unsigned char inBuf[256];
-    unsigned char outBuf[256];
-    size_t outLen;
-
-    while (fread(inBuf, 1, sizeof(inBuf), in) > 0) {
-        outLen = sizeof(outBuf);
-        if (EVP_PKEY_encrypt(ctx, outBuf, &outLen, inBuf, sizeof(inBuf)) <= 0) handleErrors();
-        fwrite(outBuf, 1, outLen, out);
-    }
-
-    EVP_PKEY_CTX_free(ctx);
-    EVP_PKEY_free(pkey);
-    fclose(in);
-    fclose(out);
-}
-
-void decryptFile(const char *infile, const char *outfile) {
-    FILE *in = fopen(infile, "rb");
-    FILE *out = fopen(outfile, "wb");
-    EVP_PKEY *pkey = loadRSAKey(0);  // Load private key
-
-    if (!in || !out || !pkey) handleErrors();
-
-    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(pkey, NULL);
-    if (!ctx || EVP_PKEY_decrypt_init(ctx) <= 0) handleErrors();
-
-    unsigned char inBuf[256];
-    unsigned char outBuf[256];
-    size_t outLen;
-
-    while (fread(inBuf, 1, sizeof(inBuf), in) > 0) {
-        outLen = sizeof(outBuf);
-        if (EVP_PKEY_decrypt(ctx, outBuf, &outLen, inBuf, sizeof(inBuf)) <= 0) handleErrors();
-        fwrite(outBuf, 1, outLen, out);
-    }
-
-    EVP_PKEY_CTX_free(ctx);
-    EVP_PKEY_free(pkey);
-    fclose(in);
-    fclose(out);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s [genkey|encrypt|decrypt] [input_file] [output_file]\n", argv[0]);
-        return 1;
-    }
-
-    if (strcmp(argv[1], "genkey") == 0) {
-        if (argc != 2) {
-            fprintf(stderr, "Usage for key generation: %s genkey\n", argv[0]);
-            return 1;
-        }
-        EVP_PKEY *pkey = createRSA(KEY_LENGTH, PUB_EXP);
-        saveRSAKey(pkey, 1);  // Save public key
-        saveRSAKey(pkey, 0);  // Save private key
-        EVP_PKEY_free(pkey);
-        printf("RSA key pair generated and saved.\n");
-    } 
-    else if (strcmp(argv[1], "encrypt") == 0) {
-        if (argc != 4) {
-            fprintf(stderr, "Usage for encryption: %s encrypt [input_file] [output_file]\n", argv[0]);
-            return 1;
-        }
-        encryptFile(argv[2], argv[3]);
-        printf("File encrypted successfully.\n");
-    } 
-    else if (strcmp(argv[1], "decrypt") == 0) {
-        if (argc != 4) {
-            fprintf(stderr, "Usage for decryption: %s decrypt [input_file] [output_file]\n", argv[0]);
-            return 1;
-        }
-        decryptFile(argv[2], argv[3]);
-        printf("File decrypted successfully.\n");
-    } 
-    else {
-        fprintf(stderr, "Invalid operation. Use 'genkey', 'encrypt', or 'decrypt'.\n");
-        return 1;
-    }
-
-    return 0;
-}
-
